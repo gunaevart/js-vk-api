dd3f2c316342fa9d49b9f5021fb7bfcec21bd21e
gunaevart <gunaevart@mail.ru>
2021-07-22 09:35:55 +0300

    Добавление

--- vk-bot.js
+++ vk-bot.js
@@ -1,5 +1,5 @@
 const VkBot = require('node-vk-bot-api');
-let TOKEN = "6334a5aaab239d621ed6032573cda00c99e524717255f27027c183ec708e405aa23e7ebb104e321f8159c"
+let TOKEN = "6334a5aaab239d621ed6032573cda00c99e524717255f27027c183ec708e405aa23e7ebb104e321f8159c" // Токен
 
 const bot = new VkBot(TOKEN);
 
